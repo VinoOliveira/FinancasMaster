79be6de5ae2706fb12172cfbe4260058033b7967
Vinicius <viniciushenrique3344@gmail.com>
2023-11-22 19:28:18 -0300

    reestruturação do projeto para os padroes do spring

---
+++ src/main/java/com/api/financasMaster/FinancasMasterApplication.java
@@ -0,0 +1,13 @@
+package com.api.financasMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/financasMaster/FinancasMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.financasMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

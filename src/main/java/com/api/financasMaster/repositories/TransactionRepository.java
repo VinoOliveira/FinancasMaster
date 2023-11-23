d67a898b037779b7e2b140323a4f2523bfbe1c0a
Vinicius <viniciushenrique3344@gmail.com>
2023-11-23 16:08:39 -0300

    feat: adding a date filter to transactions

--- src/main/java/com/api/financasMaster/repositories/TransactionRepository.java
+++ src/main/java/com/api/financasMaster/repositories/TransactionRepository.java
@@ -1,10 +1,14 @@
 package com.api.financasMaster.repositories;
 
 import com.api.financasMaster.domain.transaction.Transaction;
+import com.api.financasMaster.domain.transaction.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    Optional<Transaction> FindByUserId(Integer id);
+    Transaction findByUserId(Integer id);
+    List<Transaction> findByTransactionDateAndType(LocalDate date, TransactionType type);
 }

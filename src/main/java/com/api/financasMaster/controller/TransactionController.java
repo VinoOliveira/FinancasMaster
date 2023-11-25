a21362d3b6d755469b469bb86a84ca19d9dc2821
Vinicius <viniciushenrique3344@gmail.com>
2023-11-25 11:20:49 -0300

    feat: Addition of the TransactionController class.

---
+++ src/main/java/com/api/financasMaster/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package com.api.financasMaster.controller;
+
+import com.api.financasMaster.domain.transaction.Transaction;
+import com.api.financasMaster.dto.TransactionDTO;
+import com.api.financasMaster.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    @Autowired
+    TransactionService transactionService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction){
+        try{
+            Transaction newTransaction = transactionService.validateRegisterTransaction(transaction);
+            return new ResponseEntity<>(newTransaction,HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}

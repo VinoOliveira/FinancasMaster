06fdf57530327b37844756ae71324deede5bc874
Vinicius <viniciushenrique3344@gmail.com>
2023-11-26 09:29:44 -0300

    refactor: method singIn now return Integer, Addition of the convertUser method that converts a user to a UserTransactionDTO.

--- src/main/java/com/api/financasMaster/services/UserService.java
+++ src/main/java/com/api/financasMaster/services/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
     @Autowired
     private TransactionService transactionService;
 
+    //valida se o email informado no ato do cadastro ja existe no banco de dados
     public User validateSingUpUser(UserDTO data) throws EmailAlreadyRegisteredException {
         if (userRepository.findByEmail(data.email()).isPresent()) {
             throw new EmailAlreadyRegisteredException("Email already registered");
@@ -36,23 +37,26 @@ public class UserService {
             return user;
         }
     }
-
-    public UserTransactionDTO validateLogin(LoginRequest loginRequest) throws EmailNotFoundException, IncorrectPasswordException {
+    //valida se as informações passadas para fazer login estão de acordo com os dados cadastrados
+    public Integer validateLogin(LoginRequest loginRequest) throws EmailNotFoundException, IncorrectPasswordException {
         User user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(() -> new EmailNotFoundException("Email not found"));
 
         if (!loginRequest.getPassword().equals(user.getPassword())) {
             throw new IncorrectPasswordException("Incorrect password");
+        }else{
+            return user.getId();
         }
+    }
+    public User findUserById(Integer id) throws UserNotFoundException {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
 
+    //converte um objeto do tipo User em UserTransactionDTO
+    public UserTransactionDTO convertUser (User user){
         List<TransactionDTO> transactions = transactionService.findTransactionByUserId(user.getId());
         BigDecimal totalProfit = transactionService.sum(transactionService.findTransactionsByTypeToday(user.getId(), TransactionType.PROFIT));
         BigDecimal totalExpense = transactionService.sum(transactionService.findTransactionsByTypeToday(user.getId(), TransactionType.EXPENSE));
-
-        return new UserTransactionDTO(user,totalProfit, totalExpense , transactions);
-    }
-
-    public User findUserById(Integer id) throws UserNotFoundException {
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
+        return new UserTransactionDTO(user,totalProfit,totalExpense,transactions);
     }
 
     public void saveUser(User user) {

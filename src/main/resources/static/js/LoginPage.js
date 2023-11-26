d05fe4cfb7eacf9a548a18a544cc621326ad5396
Vinicius <viniciushenrique3344@gmail.com>
2023-11-26 12:22:54 -0300

    feat: login function

--- src/main/resources/static/js/LoginPage.js
+++ src/main/resources/static/js/LoginPage.js
@@ -1,33 +1,39 @@
 $(document).ready(function () {
-    $('#loginForm').submit(function (event) {
+    $('#loginForm').submit(async function (event) {
         event.preventDefault();
 
         var email = $('#email').val();
         var password = $('#password').val();
 
-        if (!isValidEmail(email)) {
-            alert("Please enter a valid email address.");
+        if (!isValidEmail(email) || password.trim() === '') {
+            alert("Please enter a valid email address and password.");
             return;
         }
 
-        $.ajax({
-            url: '/user/login',
-            type: 'POST',
-            contentType: 'application/json;charset=UTF-8',
-            data: JSON.stringify({email: email, password: password}),
-            success: function(response) {
-                console.log("Login bem-sucedido!", response);
-                window.location.href = '/home';
-            },
-            error: function(xhr) {
-                console.error("Erro durante o login:", xhr.responseText);
+        try {
+            const response = await fetch('/user/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                },
+                body: JSON.stringify({ email, password }),
+            });
 
-                var errorMessage = xhr.responseText;
-                alert(errorMessage);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
-        });
+
+            const userId = await response.json();
+            localStorage.setItem('userId', userId);
+
+            window.location.href = '/home';
+        } catch (error) {
+            console.error("Error during login:", error.message);
+            alert("An error occurred during login. Please try again.");
+        }
     });
-})
+});
+
 // Função para validar o formato do e-mail
 function isValidEmail(email) {
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
